d590c687f8ea032aa5782b8b36c890c23cd0581d
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-03 22:55:58 +0700

    Add files via upload

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh [TERKADANG MENGANDUNG NSFW! ]', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendButtonImg(m.chat, json.url, 'Istri gw nih 😳', '😅👍', 'Lagi', `.waifu`, m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifu']
 handler.tags = ['anime']
-handler.command = /^(istri)$/i
+handler.command = /^(waifu)$/i
 
-handler.limit = false
+handler.limit = true
 
 module.exports = handler
